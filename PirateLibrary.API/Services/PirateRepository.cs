6944cf1a61e71ee573047d4d8354fa51145ae21b
marcus quigley <marcusquigley@gmail.com>
2019-12-18 15:11:29 -0500

    db creation

---
+++ PirateLibrary.API/Services/PirateRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PirateLibrary.API.Services
+{
+    public class PirateRepository
+    {
+        readonly PirateLibraryContext context;
+
+        public PirateRepository(PirateLibraryContext context)
+        {
+            this.context = context;
+        }
+    }
+}
--- PirateLibrary.API/Startup.cs
+++ PirateLibrary.API/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +14,25 @@ namespace PirateLibrary.API
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+            });
+            services.AddDbContext<PirateLibraryContext>(options =>
+            {
+                options.UseSqlite("Data Source=memory;");
+              //  options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=PirateLibraryDb;Trusted_Connection=True;");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
